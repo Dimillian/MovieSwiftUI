96613d7d0d75b70fc76534cbcc5b6b5fcf0ae059
Thomas Ricouard <ricouard77@gmail.com>
2019-06-25 06:48:56 +0200

    Refactore MoviesReducer to a switch

--- MovieSwift/MovieSwift/flux/reducers/MoviesReducer.swift
+++ MovieSwift/MovieSwift/flux/reducers/MoviesReducer.swift
@@ -11,128 +11,138 @@ import Foundation
 struct MoviesReducer: Reducer {
     func reduce(state: MoviesState, action: Action) -> MoviesState {
         var state = state
-        if let action = action as? MoviesActions.SetPopular {
+        switch action {
+        case let action as MoviesActions.SetPopular:
             if action.page == 1 {
                 state.popular = action.response.results.map{ $0.id }
             } else {
                 state.popular.append(contentsOf: action.response.results.map{ $0.id })
             }
-            for (_, value) in action.response.results.enumerated() {
-                state.movies[value.id] = value
-            }
-        } else if let action = action as? MoviesActions.SetTopRated {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetTopRated:
             if action.page == 1 {
                 state.topRated = action.response.results.map{ $0.id }
             } else {
                 state.topRated.append(contentsOf: action.response.results.map{ $0.id })
             }
-            for (_, value) in action.response.results.enumerated() {
-                state.movies[value.id] = value
-            }
-        } else if let action = action as? MoviesActions.SetUpcoming {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetUpcoming:
             if action.page == 1 {
                 state.upcoming = action.response.results.map{ $0.id }
             } else {
                 state.upcoming.append(contentsOf: action.response.results.map{ $0.id })
             }
-            for (_, value) in action.response.results.enumerated() {
-                state.movies[value.id] = value
-            }
-        } else if let action = action as? MoviesActions.SetNowPlaying {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetNowPlaying:
             if action.page == 1 {
                 state.nowPlaying = action.response.results.map{ $0.id }
             } else {
                 state.nowPlaying.append(contentsOf: action.response.results.map{ $0.id })
             }
-            for (_, value) in action.response.results.enumerated() {
-                state.movies[value.id] = value
-            }
-        } else if let action = action as? MoviesActions.SetDetail {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetDetail:
             state.movies[action.movie] = action.response
-        } else if let action = action as? MoviesActions.SetRecommended {
+            
+        case let action as MoviesActions.SetRecommended:
             state.recommended[action.movie] = action.response.results.map{ $0.id }
-            for (_, value) in action.response.results.enumerated() {
-                state.movies[value.id] = value
-            }
-        } else if let action = action as? MoviesActions.SetSimilar {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetSimilar:
             state.similar[action.movie] = action.response.results.map{ $0.id }
-            for (_, value) in action.response.results.enumerated() {
-                state.movies[value.id] = value
-            }
-        } else if let action = action as? MoviesActions.SetSearch {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetSearch:
             if action.page == 1 {
                 state.search[action.query] = action.response.results.map{ $0.id }
             } else {
                 state.search[action.query]?.append(contentsOf: action.response.results.map{ $0.id })
             }
-            for movie in action.response.results {
-                state.movies[movie.id] = movie
-            }
-        } else if let action = action as? MoviesActions.SetSearchKeyword {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetSearchKeyword:
             state.searchKeywords[action.query] = action.response.results
-        } else if let action = action as? MoviesActions.addToWishlist {
+            
+        case let action as MoviesActions.addToWishlist:
             state.wishlist.insert(action.movie)
-        } else if let action = action as? MoviesActions.removeFromWishlist {
+            
+        case let action as MoviesActions.removeFromWishlist:
             state.wishlist.remove(action.movie)
-        } else if let action = action as? MoviesActions.addToSeenlist {
+            
+        case let action as MoviesActions.addToSeenlist:
             state.seenlist.insert(action.movie)
-        } else if let action = action as? MoviesActions.removeFromSeenlist {
+            
+        case let action as MoviesActions.removeFromSeenlist:
             state.seenlist.remove(action.movie)
-        } else if let action = action as? MoviesActions.SetMovieForGenre {
+            
+        case let action as MoviesActions.SetMovieForGenre:
             state.withGenre[action.genre.id] = action.response.results.map{ $0.id }
-            for movie in action.response.results {
-                if state.movies[movie.id] == nil {
-                    state.movies[movie.id] = movie
-                }
-            }
-        } else if let action = action as? MoviesActions.SetRandomDiscover {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetRandomDiscover:
             if state.discover.isEmpty {
                 state.discover = action.response.results.map{ $0.id }
             } else if state.discover.count < 10 {
                 state.discover.insert(contentsOf: action.response.results.map{ $0.id }, at: 0)
             }
-            for movie in action.response.results {
-                if state.movies[movie.id] == nil {
-                    state.movies[movie.id] = movie
-                }
-            }
+            state = mergeMovies(movies: action.response.results, state: state)
             state.discoverFilter = action.filter
-        } else if let action = action as? MoviesActions.SetMovieReviews {
+            
+        case let action as MoviesActions.SetMovieReviews:
             state.reviews[action.movie] = action.response.results
-        } else if let action = action as? MoviesActions.SetMovieWithCrew {
+            
+        case let action as MoviesActions.SetMovieWithCrew:
             state.withCrew[action.crew] = action.response.results.map{ $0.id }
-            for movie in action.response.results {
-                if state.movies[movie.id] == nil {
-                    state.movies[movie.id] = movie
-                }
-            }
-        } else if let action = action as? MoviesActions.SetMovieKeywords {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as MoviesActions.SetMovieKeywords:
             state.movies[action.movie]?.keywords = action.keywords
-        } else if let action = action as? MoviesActions.SetMovieWithKeyword {
+            
+        case let action as MoviesActions.SetMovieWithKeyword:
             state.withKeywords[action.keyword] = action.response.results.map{ $0.id }
-            for movie in action.response.results {
-                if state.movies[movie.id] == nil {
-                    state.movies[movie.id] = movie
-                }
-            }
-        } else if let action = action as? MoviesActions.AddCustomList {
+            state = mergeMovies(movies: action.response.results, state: state)
+            
+        case let action as  MoviesActions.AddCustomList:
             state.customLists.append(action.list)
-        } else if let action = action as? MoviesActions.RemoveCustomList {
+            
+        case let action as  MoviesActions.RemoveCustomList:
             state.customLists.removeAll{ $0.id == action.list }
-        } else if action is MoviesActions.PopRandromDiscover {
+            
+        case _ as  MoviesActions.PopRandromDiscover:
             _ = state.discover.popLast()
-        } else if let action = action as? MoviesActions.PushRandomDiscover {
+        case let action as  MoviesActions.PushRandomDiscover:
             state.discover.append(action.movie)
-        } else if action is MoviesActions.ResetRandomDiscover {
+            
+        case _ as  MoviesActions.ResetRandomDiscover:
             state.discoverFilter = nil
             state.discover = []
-        } else if let action = action as? MoviesActions.SetMovieImages {
+            
+        case let action as  MoviesActions.SetMovieImages:
             state.movies[action.movie]?.posters = action.response.posters
             state.movies[action.movie]?.backdrops = action.response.backdrops
-        } else if let action = action as? MoviesActions.SetGenres {
+            
+        case let action as  MoviesActions.SetGenres:
             state.genres = action.genres
             state.genres.insert(Genre(id: -1, name: "Random"), at: 0)
+            
+        default:
+            break
         }
+        
+
         return state
     }
+}
+
+private func mergeMovies(movies: [Movie], state: MoviesState) -> MoviesState {
+    var state = state
+    for movie in movies {
+        if state.movies[movie.id] == nil {
+            state.movies[movie.id] = movie
+        }
+    }
+    return state
 }
