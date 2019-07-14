dfb717e838cdc90bf0123ab7d6298c2e0f2a7626
Thomas Ricouard <ricouard77@gmail.com>
2019-07-14 10:36:34 +0200

    ImagesCarouselView: Support zoom on poster

--- MovieSwift/MovieSwift/views/shared/images/ImagesCarouselView.swift
+++ MovieSwift/MovieSwift/views/shared/images/ImagesCarouselView.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ImagesCarouselView : View {
     let posters: [ImageData]
     @Binding var selectedPoster: ImageData?
+    @State var innerSelectedPoster: ImageData?
     
     func computeCarouselPosterScale(width: Length, itemX: Length) -> Length {
         let trueX = itemX - (width/2 - 250/3)
@@ -23,35 +24,59 @@ struct ImagesCarouselView : View {
         return 1
     }
     
-    
     var body: some View {
         GeometryReader { reader in
             ZStack(alignment: .center) {
-                ScrollView(.horizontal, showsIndicators: false) {
-                    HStack(spacing: 200) {
-                        ForEach(self.posters) { poster in
-                            GeometryReader { reader2 in
-                                BigMoviePosterImage(imageLoader: ImageLoader(path: poster.file_path,
-                                                                             size: .medium))
-                                    .scaleEffect(self.selectedPoster == nil ?
-                                        .zero :
-                                        self.computeCarouselPosterScale(width: reader.frame(in: .global).width,
-                                                                        itemX: reader2.frame(in: .global).midX),
-                                                 anchor: .center)
-                                    .zIndex(1)
-                                    .tapAction {
-                                        withAnimation {
-                                            self.selectedPoster = nil
+                Group {
+                    if self.innerSelectedPoster != nil {
+                        BigMoviePosterImage(imageLoader: ImageLoader(path: self.innerSelectedPoster!.file_path,
+                                                                     size: .medium))
+                            .position(x: reader.frame(in: .local).midX,
+                                      y: reader.frame(in: .local).midY)
+                            .scaleEffect(1.3)
+                            .tapAction {
+                                withAnimation {
+                                    self.innerSelectedPoster = nil
+                                }
+                        }
+                    } else {
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack(spacing: 200) {
+                                ForEach(self.posters) { poster in
+                                    GeometryReader { reader2 in
+                                        BigMoviePosterImage(imageLoader: ImageLoader(path: poster.file_path,
+                                                                                     size: .medium))
+                                            .scaleEffect(self.selectedPoster == nil ?
+                                                .zero :
+                                                self.computeCarouselPosterScale(width: reader.frame(in: .global).width,
+                                                                                itemX: reader2.frame(in: .global).midX),
+                                                         anchor: .center)
+                                            .zIndex(1)
+                                            .tapAction {
+                                                withAnimation {
+                                                    self.innerSelectedPoster = poster
+                                                }
                                         }
+                                    }
                                 }
                             }
                         }
+                        .position(x: reader.frame(in: .global).midX,
+                                  y: reader.frame(in: .local).midY)
+                        
+                        Button(action: {
+                            self.selectedPoster = nil
+                        }) {
+                            Circle()
+                                .strokeBorder(Color.red, lineWidth: 1)
+                                .background(Image(systemName: "xmark").foregroundColor(.red))
+                                .frame(width: 50, height: 50)
+                            
+                        }
+                        .position(x: reader.frame(in: .local).midX,
+                                  y: reader.frame(in: .local).maxY - 80)
+                       
                     }
-                    }
-                    .position(x: reader.frame(in: .global).midX,
-                              y: reader.frame(in: .local).midY)
-                    .tapAction {
-                        self.selectedPoster = nil
                 }
             }
         }
