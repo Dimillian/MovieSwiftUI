ef7eade2a33b484824f73b351352dbff819ff017
Kaya <kayathom@gmail.com>
2019-07-14 21:53:42 -0700

    add whitespace to imageScale modifier

--- MovieSwift/MovieSwift/views/components/home/HomeView.swift
+++ MovieSwift/MovieSwift/views/components/home/HomeView.swift
@@ -33,7 +33,7 @@ struct TabbarView: View {
     func tabbarItem(text: String, image: String) -> some View {
         VStack {
             Image(systemName: image)
-            .imageScale(.large)
+                .imageScale(.large)
             Text(text)
         }
     }
