48331e194fd8d4b4512c28149bcdf0b33bd50373
Thomas Ricouard <ricouard77@gmail.com>
2019-07-26 13:51:53 +0200

    Add animation on PeopleRow

--- MovieSwift/MovieSwift/views/components/moviesList/rows/PeopleRow.swift
+++ MovieSwift/MovieSwift/views/components/moviesList/rows/PeopleRow.swift
@@ -12,28 +12,47 @@ import SwiftUIFlux
 struct PeopleRow : ConnectedView {
     struct Props {
         let people: People
+        let isInFanClub: Bool
     }
     
     let peopleId: Int
     
     func map(state: AppState, dispatch: @escaping DispatchFunction) -> Props {
-        Props(people: state.peoplesState.peoples[peopleId]!)
+        Props(people: state.peoplesState.peoples[peopleId]!,
+              isInFanClub: state.peoplesState.fanClub.contains(peopleId))
+    }
+    
+    private var fanClubIcon: some View {
+        Image(systemName: "star.circle")
+            .imageScale(.large)
+            .foregroundColor(.steam_gold)
+            .transition(AnyTransition.scale
+                .combined(with: .opacity))
+            .animation(.interpolatingSpring(stiffness: 80, damping: 10))
     }
     
     func body(props: Props) -> some View {
         HStack {
             PeopleImage(imageLoader: ImageLoader(path: props.people.profile_path, size: .cast))
             VStack(alignment: .leading) {
-                Text(props.people.name)
-                    .font(.FjallaOne(size: 20))
-                    .foregroundColor(.steam_gold)
-                    .lineLimit(1)
+                HStack(alignment: .firstTextBaseline, spacing: 4) {
+                    if props.isInFanClub {
+                        fanClubIcon
+                    }
+                    Text(props.people.name)
+                        .font(.FjallaOne(size: 20))
+                        .foregroundColor(.steam_gold)
+                        .lineLimit(1)
+                        .animation(.spring())
+                }
                 Text(props.people.knownForText ?? "")
                     .foregroundColor(.secondary)
                     .font(.subheadline)
                     .lineLimit(3)
                     .truncationMode(.tail)
-            }.padding(.leading, 8)
+                    .frame(height: 40)
+            }
+            .padding(.leading, 8)
         }.padding(.top)
         .padding(.bottom)
         .contextMenu{ PeopleContextMenu(people: peopleId) }
