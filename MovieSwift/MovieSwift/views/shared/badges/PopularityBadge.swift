8b19083f581910076acf744fd1d7e81ef4863fb3
Thomas Ricouard <ricouard77@gmail.com>
2019-10-09 14:25:09 +0200

    Fix popularity badge

--- MovieSwift/MovieSwift/views/shared/badges/PopularityBadge.swift
+++ MovieSwift/MovieSwift/views/shared/badges/PopularityBadge.swift
@@ -29,12 +29,11 @@ struct PopularityBadge : View {
     var overlay: some View {
         ZStack {
             Circle()
-              .stroke(Color.secondary, lineWidth: 2)
+                .stroke(Color.secondary, lineWidth: 2)
             Circle()
                 .trim(from: 0,
                       to: isDisplayed ? CGFloat(score) / 100 : 0)
                 .stroke(scoreColor, lineWidth: 2)
-                .shadow(color: scoreColor, radius: 4)
                 .animation(Animation.interpolatingSpring(stiffness: 60, damping: 10).delay(0.1))
         }
         .rotationEffect(.degrees(-90))
@@ -49,6 +48,7 @@ struct PopularityBadge : View {
                 .foregroundColor(.clear)
                 .frame(width: 40)
                 .overlay(overlay)
+                .shadow(color: scoreColor, radius: 4)
             Text("\(score)%")
                 .font(Font.system(size: 10))
                 .fontWeight(.bold)
@@ -61,7 +61,12 @@ struct PopularityBadge : View {
 #if DEBUG
 struct PopularityBadge_Previews : PreviewProvider {
     static var previews: some View {
-        PopularityBadge(score: 80)
+        VStack {
+            PopularityBadge(score: 80)
+            PopularityBadge(score: 10)
+            PopularityBadge(score: 30)
+            PopularityBadge(score: 50)
+        }
     }
 }
 #endif
