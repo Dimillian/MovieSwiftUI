b6c12860572eb16754919aa46f077c58b0bdfcd4
zhongzheng <danglu999@gmail.com>
2021-06-01 20:10:59 +0800

    add filter to avoid loading same data duplicately

--- MovieSwift/MovieSwift/views/components/fan club/FanClubHome.swift
+++ MovieSwift/MovieSwift/views/components/fan club/FanClubHome.swift
@@ -20,7 +20,7 @@ struct FanClubHome: ConnectedView {
     
     func map(state: AppState , dispatch: @escaping DispatchFunction) -> Props {
         Props(peoples: state.peoplesState.fanClub.map{ $0 }.sorted(),
-              popular: Array(Set(state.peoplesState.popular))
+              popular: state.peoplesState.popular
                 .filter{ !state.peoplesState.fanClub.contains($0) }
                 .sorted() { state.peoplesState.peoples[$0]!.name < state.peoplesState.peoples[$1]!.name },
               dispatch: dispatch)
@@ -60,7 +60,9 @@ struct FanClubHome: ConnectedView {
             .animation(.spring())
         }
         .onAppear {
-            props.dispatch(PeopleActions.FetchPopular(page: self.currentPage))
+            if self.currentPage == 1{
+                props.dispatch(PeopleActions.FetchPopular(page: self.currentPage))
+            }
         }
     }
 }
